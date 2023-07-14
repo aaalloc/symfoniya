45a80bef1217475b4c96fae432a5b905b47a669f
Siirko <alexander.yanovskyy@gmail.com>
2023-07-14 22:30:13 +0200

    reworked add music modal

--- src/components/AddMusic.tsx
+++ src/components/AddMusic.tsx
@@ -8,6 +8,15 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { DialogClose } from "@radix-ui/react-dialog";
@@ -15,6 +24,7 @@ import { open } from '@tauri-apps/api/dialog';
 import { appDataDir } from '@tauri-apps/api/path';
 import { useState } from "react"
 import { invoke } from "@tauri-apps/api/tauri"
+import { type } from "os"
 
 
 
@@ -22,7 +32,7 @@ export function AddMusic() {
     const [buttonDesc, setButtonDesc] = useState<string>(
         "Waiting to be clicked. This calls 'on_button_clicked' from Rust.",
     )
-    const [path, setPath] = useState<string | string[] | null>('None');
+    const [path, setPath] = useState<string | string[]>('None');
     const choose_path = async () => {
         const import_dialog = await import('@tauri-apps/api/dialog');
         const import_path = await import('@tauri-apps/api/path');
@@ -31,7 +41,12 @@ export function AddMusic() {
             multiple: true,
             defaultPath: await import_path.audioDir(),
         });
-        setPath(path);
+        if (typeof path == null) {
+            setPath("None");
+        }
+        else {
+            setPath(path);
+        }
     }
 
     const handle_submit = () => {
@@ -51,18 +66,32 @@ export function AddMusic() {
             <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
                     <DialogTitle>Add musics</DialogTitle>
-                    <DialogDescription>
-                        Select one or multiples musics according to a folder path.
-                    </DialogDescription>
                 </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                            Path
-                        </Label>
-                        <Input onClick={choose_path} id="path" value={path == null ? "Something happened ..." : path} className="col-span-3" />
+                {/* value={path == null ? "Something happened ..." : path} */}
+                {path == null || 'None' ? <div onClick={choose_path} className="cursor-pointer grid h-[200px] items-center justify-center rounded-md border border-dashed text-sm">
+                    <div className="items-center">
+                        <p className="text-sm text-muted-foreground">Select a or multiple folder path </p>
                     </div>
-                </div>
+                </div> :
+                    <div className="grid h-[200px] items-center justify-center rounded-md border text-sm">
+                        <Table>
+                            <TableCaption>Path selected</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Path</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {typeof path == "string" ? <TableRow>
+                                    <TableCell>{path}</TableCell>
+                                </TableRow> : path.map((value) => {
+                                    return <TableRow>
+                                        <TableCell>{value}</TableCell>
+                                    </TableRow>
+                                })}
+                            </TableBody>
+                        </Table>
+                    </div>}
                 <DialogFooter>
                     <DialogClose>
                         <Button onClick={handle_submit} disabled={path != "None" && (typeof path != null) ? false : true} type="submit">Import musics</Button>
