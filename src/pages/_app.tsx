68be66ba664c0152eeb61aa19a5afdac7bd0f8ad
Siirko <alexander.yanovskyy@gmail.com>
2023-07-15 11:29:08 +0200

    added toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.scss"
 
 import type { AppProps } from "next/app"
 import { ThemeProvider } from 'next-themes'
+import { Toaster } from "@/components/ui/toaster"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Component {...pageProps} />
+      <Toaster />
     </ThemeProvider>
   )
 }
