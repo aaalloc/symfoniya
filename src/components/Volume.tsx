750191106ee6c7e0825e636c396dc7de2c075cfc
Alexander Yanovskyy <Siirko@github.com>
2023-07-24 01:35:45 +0200

    save audio slider bar

--- src/components/Volume.tsx
+++ src/components/Volume.tsx
@@ -3,11 +3,14 @@ import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
 import { Slider } from "./ui/slider"
 import { Button } from "./ui/button"
 import { Volume2 } from "lucide-react"
+import { useState } from "react"
+
 
 export default function VolumeButton() {
-
+    const [volume, setVolume] = useState([100])
     const set_volume = async (volume: number[]) => {
         await invoke("set_volume", { volume: volume[0] / 100 })
+        setVolume(volume)
         return volume
     }
 
@@ -18,7 +21,7 @@ export default function VolumeButton() {
             </Button>
         </PopoverTrigger>
         <PopoverContent>
-            <Slider defaultValue={[100]} max={100} step={1} onValueChange={(value) => set_volume(value)} />
+            <Slider defaultValue={volume} max={100} step={1} onValueChange={(value) => set_volume(value)} />
         </PopoverContent>
     </Popover>
 
