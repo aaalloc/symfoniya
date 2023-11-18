b9ac3605bd2c7e0b6686ef6051cb24792ee48b1a
Siirko <alexander.yanovskyy@gmail.com>
2023-11-18 17:51:51 +0100

    added system tray icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -20,6 +20,7 @@ use music::{
 };
 
 use rodio::OutputStream;
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 use tauri::{Manager, State};
 use tauri_plugin_log::{fern::colors::ColoredLevelConfig, LogTarget};
 use tokio::sync::mpsc;
@@ -36,6 +37,27 @@ fn main() {
     let (async_process_output_tx, mut async_process_output_rx) = mpsc::channel(1);
 
     tauri::Builder::default()
+        .system_tray(
+            SystemTray::new().with_menu(
+                SystemTrayMenu::new()
+                    .add_item(CustomMenuItem::new("open".to_string(), "Open Symfoniya"))
+                    .add_native_item(SystemTrayMenuItem::Separator)
+                    .add_item(CustomMenuItem::new("quit".to_string(), "Quit")),
+            ),
+        )
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "open" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .manage(arc_player)
         .manage(DbState {
             db: Default::default(),
@@ -95,6 +117,13 @@ fn main() {
             });
 
             Ok(())
+        })
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
