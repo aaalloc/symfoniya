0292bd4f0174bc3a07150d75674c01110a0a2d12
Franck G <gutmann.franck@outlook.fr>
2023-11-20 22:26:06 +0100

    fix(progess): thumb + init state

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,20 +12,21 @@ const Progress = React.forwardRef<
     <SliderPrimitive.Root
       ref={ref}
       className={cn(
-        "relative flex w-full touch-none select-none items-center",
+        "relative flex w-full touch-none select-none items-center group/item",
         className,
       )}
       {...props}
     >
-      <SliderPrimitive.Track className="cursor-pointer group/item relative h-2 w-full grow overflow-hidden bg-gradient-to-r to-[#00C5DF] via-[#FFC700] from-[#F2371F]">
+      <SliderPrimitive.Track className="cursor-pointer  relative h-2 w-full grow overflow-hidden bg-gradient-to-r to-[#00C5DF] via-[#FFC700] from-[#F2371F]">
         <SliderPrimitive.Range className="absolute h-full transition-all bg-transparent" />
+        <div
+          className={
+            "absolute top-0 right-0 bg-secondary h-full transition-all cursor-pointer w-full"
+          }
+          style={{ width: `${mask}%` }}
+        />
       </SliderPrimitive.Track>
-      <div
-        className={
-          "absolute top-0 right-0 bg-secondary h-full transition-all cursor-pointer"
-        }
-        style={{ width: `${mask}%` }}
-      />
+
       <SliderPrimitive.Thumb className="invisible group-hover/item:visible block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 z-20" />
     </SliderPrimitive.Root>
   )
